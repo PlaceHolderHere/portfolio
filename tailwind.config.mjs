76e6e209178b727f5086ca67694fc96ac172d097
PlaceHolderHere <holderhereinplace@gmail.com>
2024-05-17 19:57:59 +0800

    Layouts WIP

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'page-color': '#E8EBFF',
+				'dominant-color': '#6A67CE',
+				'compliment-color': '#B0ADE4',
+				'accent-color': '#2C3E50',
+				'button-color': '#6DBAA1',	
+			}
+		},
 	},
 	plugins: [],
 }
